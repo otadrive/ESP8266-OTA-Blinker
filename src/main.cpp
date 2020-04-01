ae06e26587f887fd98bc2e214e9c5d0cc84099ac
mostafa heidari <mostafa.hk@gmail.com>
2020-04-02 00:42:42 +0430

    V1.0.0.1

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <Arduino.h>
 #include <ESP8266httpUpdate.h>
-String version = "1.0.0.0";
+String version = "1.0.0.1";
 char SSID[33] = "SohaDevice";
 char PASS[33] = "DamkpKCk";
 void doUpdate();
@@ -31,12 +31,12 @@ void loop()
 {
   // Lets blink
   digitalWrite(2, 1);
-  delay(50);
+  delay(250);
   digitalWrite(2, 0);
-  delay(50);
+  delay(250);
 
   updateCheckTimer++;
-  if(updateCheckTimer > 100)
+  if(updateCheckTimer > 50)
   {
     updateCheckTimer = 0;
     doUpdate();
